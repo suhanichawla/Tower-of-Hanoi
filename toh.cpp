9240ca574ae628f2de868963c5e0e8802a4647a3
apurva0411 <twinklestar039@gmail.com>
2019-10-02 17:43:10 +0530

    update

--- toh.cpp
+++ toh.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 
-void toh(int n,int s,int d,int h)
+int toh(int n,int s,int d,int h)
 {
     if(n==0)
     {
-        return;
+        return 1;
     }
    toh(n - 1, s, h, d); //2 shells can go from st to ht using dt ->faith  smaller problem
    cout<<"["<<s<<"-"<<d<<"]"<<n<<endl;//exp<=>faith  print n st to dt 
@@ -15,5 +15,7 @@ void toh(int n,int s,int d,int h)
 int main(int argc,char** argv)
 {
     int shells=3;
-    toh(3,1,2,3);
+   int res= toh(3,1,2,3);
+   cout<<res;
+   
 }
